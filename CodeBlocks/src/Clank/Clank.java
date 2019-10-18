717022f205d19c841a283c11dee73273dd668ad6
I-am-someth1ng <56703935+I-am-someth1ng@users.noreply.github.com>
2019-10-17 19:21:51 -0500

    Add files via upload
    Base code.

---
+++ CodeBlocks/src/Clank/Clank.java
@@ -0,0 +1,34 @@
+package Clank;
+
+import java.util.Scanner;
+
+public class Clank
+{
+    
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner scnr = new Scanner(System.in);
+        double initialMealPoints = 700;
+        double currentMealPoints = 0;
+        double mealPointsSpentAWeek = 0;
+        int swipesAWeek = 6;
+        int startingWeek = 1;
+        int endingWeek = 16;
+
+
+
+    
+        System.out.println("Thank you for using the meal point calculator.  Please enter how many weeks are in your current semester (default is 16 weeks):");    
+    
+    }
+}
+/*
+What it should do:
+How many points to spend on a meal
+How many to spend a week
+Take in how many meals a person plans on eating at on campus restaurants
+(optional) give a response to what a person says is their favorite restaurant on campus
+Take in how many weeks into the semester they are
+Take in how many meal points they started with and how many they have now
+provide meal plan options and let them choose one or choose a custom meal plan
+*/
